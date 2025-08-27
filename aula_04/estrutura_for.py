ee5e9bb4c7602b9adf20cd66ab3edc2399ec0a7d
Emanuel Nunes <emanuel.n07@gmail.com>
2025-08-27 11:47:46 -0300

    Aula04

---
+++ aula_04/estrutura_for.py
@@ -0,0 +1,10 @@
+# for = Voce sabe quantas vezes vai acontecer
+# É usado para iterar sobre elementos de uma sequencia, ou para repetir blocos determinadas vezes.
+
+#Exiba todos o spordutos que estão na lista. Exiba com a seguite estrutura: "Temos disponivel o produto".
+
+lista = ["Smatphone", "SmartTV", "Geladeira", "Video Game", "Fogão", "Relogio", "Notebook"]
+print("🛒 Produtos disponiveis em estoque: ")
+
+for produto in lista:
+    print(f"Temos disponivel o/a {produto}")
---
+++ aula_04/estrutura_while.py
@@ -0,0 +1,12 @@
+# while = vc não sabe quantas vezes vai acontecer
+# Executa um bloco de codigo enquanto a condição for verdadeira(TRUE).
+
+
+# Enquanto o numero for menor que 5, informe "Numero baixo"
+
+numero = 3
+
+while numero < 5:
+    print('Numero baixo')
+    # numero -= 1 # numero = numero - 1
+    numero += 1 # numero = numero + 1
